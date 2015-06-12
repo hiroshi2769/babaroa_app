e782512464ad26bd5e5abc40add8c3a79101ce12
nakayamahiroshi <hiroshi2769@icloud.com>
2015-06-12 09:53:02 +0900

    演習終わったよ

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+   let(:base_title) { "ニセコイバンバン" }
+
    describe "Home page" do
      it "ニセコイかも知れない 'Babaroa App'" do
      visit '/static_pages/home'
@@ -9,19 +12,20 @@ describe "StaticPages" do
 
      it "正しいタイトル持ってるだろ！" do
      visit '/static_pages/home'
-     expect(page).to have_title("ニセコイ | 小野寺")
+     expect(page).to have_title("#{base_title} | 小野寺")
      end
    
    end
+
    describe "Help page" do
      it "あー楽様かも知れない 'Babaroa App'" do
      visit '/static_pages/help'
-     expect(page).to have_content('らく様')
+     expect(page).to have_content('あーらく様')
      end
 
      it "正しいタイトル持ってるだろ！らく様" do
      visit '/static_pages/help'
-     expect(page).to have_title("ニセコイ | あーらく様")
+     expect(page).to have_title("#{base_title} | あーらく様")
      end
    
    end
@@ -31,11 +35,23 @@ describe "StaticPages" do
      visit '/static_pages/about'
      expect(page).to have_content('千棘')
      end
-   end
 
      it "正しいタイトル持ってるだろ！千棘" do
      visit '/static_pages/about'
-     expect(page).to have_title("ニセコイ | 千棘")
+     expect(page).to have_title("#{base_title} | 千棘")
      end
+   end
    
+   describe "マリカの強襲 page" do
+     it "本当は身体が弱い万里花'About ニセコイ'" do
+     visit '/static_pages/contact'
+     expect(page).to have_content('万里花')
+     end
+
+     it "正しいタイトル持ってるだろ！万里花" do
+     visit '/static_pages/contact'
+     expect(page).to have_title("#{base_title} | 万里花")
+     end
+   end
+
 end
